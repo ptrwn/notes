1e07a479d2e9d6607c74ce4481c04e3fd57dcae6
unknown <1374688@gmail.com>
2020-04-08 21:11:07 +0800

    changed category from db_choice to model, added note uuid functions

--- notekeeper/models.py
+++ notekeeper/models.py
@@ -1,28 +1,34 @@
 import uuid
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
 
-CATEGORY_CHOICES= [
-    ('link', 'Link'),
-    ('note', 'Note'),
-    ('memo', 'Memo'),
-    ('todo', 'To do'),
-    ]
+    def __str__(self):
+        return self.name
 
 
 class Note(models.Model):
     header = models.CharField(max_length=200)
     body = models.TextField()
     created_on = models.DateTimeField(default=timezone.now)
-    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(null=True, editable=False)
+    created_by = models.ForeignKey(User, related_name='own_note', on_delete=models.CASCADE)
     is_favorite = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, blank=False, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.header
 
+    def add_uuid(self):
+        self.uuid = uuid.uuid4()
+        self.save()
+        return self.uuid
 
-
+    def del_uuid(self):
+        self.uuid = None
+        self.save()
 
--- notekeeper/views.py
+++ notekeeper/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Note
 
 
 def index(request):
-    return HttpResponse("notekeeper index")
+    latest_note_list = Note.objects.order_by('-created_on')[:5]
+    output = ', '.join([n.header for n in latest_note_list])
+    return HttpResponse(output)
 
 def note_details(request, note_id):
     return HttpResponse("You're looking at note %s." % note_id)
+
