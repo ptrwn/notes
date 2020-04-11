eefbe7f4b5294033c90df204f93287019a612e90
unknown <1374688@gmail.com>
2020-04-11 18:07:50 +0800

    added templates, form, new urls and views

--- notekeeper/urls.py
+++ notekeeper/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'notekeeper'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:note_id>/', views.note_details, name='note_details'),
+    path('new/', views.create_note, name='create_note'),
+    path('delete/', views.delete_note, name='delete_note'),
+    path('update/<int:note_id>/', views.update_note, name='update_note')
 ]
--- notekeeper/views.py
+++ notekeeper/views.py
@@ -1,13 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from.models import Note
+from django.template import loader
+
+from .models import Note
+from .forms import NoteForm
 
 
 def index(request):
-    latest_note_list = Note.objects.order_by('-created_on')[:5]
-    output = ', '.join([n.header for n in latest_note_list])
-    return HttpResponse(output)
+    own_note_list = Note.objects.filter(created_by=request.user)
+    template = loader.get_template('notekeeper/index.html')
+    context = {
+        "latest_note_list": own_note_list
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def update_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if not request.user.id == note.created_by.id:
+        return HttpResponse('Forbidden', status=403)
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            updated_note = Note.objects.get(id=note_id)
+            updated_note.header = form.cleaned_data['header']
+            updated_note.body = form.cleaned_data['body']
+            updated_note.is_favorite = form.cleaned_data['is_favorite']
+            updated_note.category = form.cleaned_data['category']
+
+            updated_note.save()
+            return redirect('notekeeper:note_details', note_id=updated_note.id)
+
+    form = NoteForm(initial={
+        'header': note.header,
+        'body': note.body,
+        'is_favorite': note.is_favorite,
+        'category': note.category
+    })
+    context = {
+        "form": form
+    }
+    template = loader.get_template('notekeeper/update_note.html')
+    return HttpResponse(template.render(context, request))
+
+
+def delete_note(request):
+    if request.method == 'POST':
+        note_id = request.POST['note_id']
+        note = Note.objects.get(id=int(note_id))
+        if not request.user.id == note.created_by.id:
+            return HttpResponse('Forbidden', status=403)
+        note.delete()
+        return redirect('notekeeper:index')
+    return HttpResponse("pych")
+
 
 def note_details(request, note_id):
-    return HttpResponse("You're looking at note %s." % note_id)
+    note = get_object_or_404(Note, id=note_id)
+    if not request.user.id == note.created_by.id:
+        return HttpResponse('Forbidden', status=403)
+    context = {
+        "note": note
+    }
+    template = loader.get_template('notekeeper/read.html')
+    return HttpResponse(template.render(context, request))
 
+
+def create_note(request):
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return HttpResponse('Unauthorized', status=401)
+
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            new_note = Note.objects.create(
+                header=form.cleaned_data['header'],
+                body=form.cleaned_data['body'],
+                is_favorite=form.cleaned_data['is_favorite'],
+                category=form.cleaned_data['category'],
+                created_by=request.user
+            )
+            return redirect('notekeeper:note_details', note_id=new_note.id)
+
+    form = NoteForm()
+    context = {
+        "form": form
+    }
+    template = loader.get_template('notekeeper/newnote.html')
+    return HttpResponse(template.render(context, request))
